0ae97576ee7fc5cbae208cbde4de665a42010d50
Renxia Wang <renxia.wang@gmail.com>
2015-02-25 00:21:29 -0800

    implemented parse data read in urlfilter

--- nutch/src/plugin/urlfilter-exactdup/src/java/org/apache/nutch/urlfilter/exactdup/ExactDupURLFilter.java
+++ nutch/src/plugin/urlfilter-exactdup/src/java/org/apache/nutch/urlfilter/exactdup/ExactDupURLFilter.java
@@ -1,6 +1,11 @@
 package org.apache.nutch.urlfilter.exactdup;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
@@ -10,13 +15,14 @@ import org.apache.nutch.crawl.Inlinks;
 import org.apache.nutch.crawl.LinkDbReader;
 import org.apache.nutch.net.URLFilter;
 import org.apache.nutch.segment.SegmentReader;
+import org.apache.nutch.util.HadoopFSUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -28,59 +34,140 @@ public class ExactDupURLFilter implements URLFilter {
     private static final Logger LOG = LoggerFactory
             .getLogger(ExactDupURLFilter.class);
 
+    private static final String MAPRED_INPUT_DIR = "mapred.input.dir";
+    private static final String CRAWLDB = "crawldb";
+    private static final String CURRENT = "current";
+    private static final String LINKDB = "linkdb";
+    private static final String SEGMENTS = "segments";
+    private static final String CRAWL_FETCH = "crawl_fetch";
+    private static final String CRAWL_PARSE = "crawl_parse";
+    private static final String PARSE_DATA = "parse_data";
+    private static final String PARSE_TEXT = "parse_text";
+    private static final String FILE_PROTOCOL = "file:";
+
     private Configuration conf;
+    private FileSystem fs;
     private LinkDbReader linkdbReader;
     private CrawlDbReader crawlDbReader;
     private SegmentReader segmentReader;
-    private long count = 0;
 
-    public ExactDupURLFilter() {
-        super();
+    private String basePath;
+    private Path crawlDbPath;
+    private Path linkDbPath;
+    private List<Path> segmentPaths;
+
+    private boolean initialized = false;
+    private long duplicateCounter = 0;
+
+    public ExactDupURLFilter() {}
+
+    public void init() {
         try {
-            /*linkdbReader = new LinkDbReader(conf, new Path("/home/renxia/urlfilter/nutch/runtime/local/aaa/linkdb"));
-            crawlDbReader = new CrawlDbReader();
-            segmentReader = new SegmentReader(conf, false, false, false, false, true, true);*/
+            fs = FileSystem.get(conf);
 
+            segmentPaths = Lists.newArrayList();
+
+            // get crawldb, linkdb, parse_data, parse_text paths
+            String paths = conf.get(MAPRED_INPUT_DIR);
+            generatePaths(paths);
+
+            // init readers
+            linkdbReader = new LinkDbReader(conf, linkDbPath);
+            crawlDbReader = new CrawlDbReader();
+            segmentReader = new SegmentReader(conf, false, false, false, false, true, true);
+
+            initialized = true;
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Initialization failed. ", e);
+            return;
         }
+    }
+
+    public void generatePaths(String paths) {
+        String[] pathAry = paths.split(",");
+
+        basePath = pathAry[0].replace(FILE_PROTOCOL, "").replace(String.format("%s/%s", CRAWLDB, CURRENT), "");
+
+        crawlDbPath = new Path(basePath, CRAWLDB);
+        linkDbPath = new Path(basePath, LINKDB);
+
+
+        try {
+            FileStatus[] fstats = new FileStatus[0];
+            fstats = fs.listStatus(new Path(basePath, SEGMENTS),
+                    HadoopFSUtil.getPassDirectoriesFilter(fs));
+
+            Path[] files = HadoopFSUtil.getPaths(fstats);
+            if (files != null && files.length > 0) {
+                segmentPaths.addAll(Arrays.asList(files));
+            }
+
+        } catch (IOException e) {
+            LOG.error("Unable to get segment paths", e);
+        }
+
+        LOG.info("crawldb path: " + crawlDbPath);
+        LOG.info("linkdb path: " + linkDbPath);
+        LOG.info("segment paths " + Joiner.on(",").join(segmentPaths));
     }
 
     @Override
     public String filter(String urlString) {
-        LOG.info("URlFilter ID: " + System.identityHashCode(this));
-        count++;
+
+        if (!initialized) {
+            LOG.info("Initializing URlFilter, ID: " + System.identityHashCode(this));
+            init();
+        }
 
         /*try {
+            LOG.info("Reading anchors for url: " + urlString);
             Text url = new Text(urlString);
             String[] anchors = linkdbReader.getAnchors(url);
             for (String s : anchors) {
                 LOG.info("anchors: " + s);
             }
 
+            LOG.info("Reading inlinks for url: " + urlString);
             Inlinks inLinks = linkdbReader.getInlinks(url);
             LOG.info("Inlinks: " + inLinks.toString());
-
-            CrawlDatum data = crawlDbReader.get("/home/renxia/urlfilter/nutch/runtime/local/aaa/crawldb", urlString, conf);
-            LOG.info("Crawl data: " + data.toString());
-
-            segmentReader.get(new Path("/home/renxia/urlfilter/nutch/runtime/local/aaa/aaa/segments"), new Text(urlString), new OutputStreamWriter(
-                    System.out, "UTF-8"), new HashMap<String, List<Writable>>());
-
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error("Unable to get data of " + urlString + " from linkdb", e);
+        }
+
+        try {
+            LOG.info("Reading crawldb data for url: " + urlString);
+            CrawlDatum data = crawlDbReader.get(crawlDbPath.toString(), urlString, conf);
+            LOG.info("Crawl data: " + data.toString());
+        } catch (IOException e) {
+            LOG.error("Unable to get data of " + urlString + " from crawldb", e);
+        }*/
+
+        try {
+            LOG.info("Reading parse data and text for url: " + urlString);
+            for (Path path : segmentPaths) {
+                Map<String, List<Writable>> results = Maps.newHashMap();
+                segmentReader.get(path, new Text(urlString),
+                        new OutputStreamWriter(System.out, "UTF-8"),
+                        results);
+
+                if (results.containsKey("pd")) {
+                    List<Writable> res = results.get("pd");
+                    boolean isParseDataEmpty = true;
+                    for (int i = 0; i < res.size(); i++) {
+                        isParseDataEmpty = false;
+                        LOG.info("===================================" + res.get(i));
+                    }
+
+                    if (!isParseDataEmpty) {
+                        break;
+                    }
+                }
+            }
+
         } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+            LOG.error("Unable to get data of " + urlString + " from parse_data", e);
+        }
 
-        /*Iterator it = this.conf.iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, String> entry = (Map.Entry<String, String>) it.next();
-            LOG.info("entry key: " + entry.getKey());
-            LOG.info("entry value: " + entry.getValue());
-        }*/
-
-        LOG.info("Processed " + count + " links");
         return urlString;
     }
 
